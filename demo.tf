0750bcdd7eae24aa0b53347e9027d66198626546
unknown <talktoitopa@gmail.com>
2025-11-03 17:43:55 +0100

    s3 demo

---
+++ demo.tf
@@ -0,0 +1,16 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 6.0"
+        }
+    }
+}
+
+resource "aws_s3_bucket" "example" {
+    bucket = "bologat-demo"
+    tags = {
+        Name = "bologat-bucket"
+        Environment = "Dev"
+    }
+}
